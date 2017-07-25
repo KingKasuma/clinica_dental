3ad0fd47bd37af387cebd924db87c33288c19331
KingKasuma <lanselot777hailander@gmail.com>
2017-07-25 07:11:40 -0400

    Login page view

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,13 @@ class User < ActiveRecord::Base
          :recoverable, :rememberable, :trackable, :validatable
   belongs_to :employee
   belongs_to :patient
+
+  def has_rol(rol)
+    rol.downcase!
+    resp = false
+    if self.employee.role.nombre.downcase == rol
+      resp = true
+    end
+    resp
+  end
 end
