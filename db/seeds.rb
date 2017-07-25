3694d67d7d3d2d478dcdb781e62ec3b221d96287
KingKasuma <lanselot777hailander@gmail.com>
2017-07-25 07:18:30 -0400

    Data Base seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+a = Role.create! :nombre => 'Administrador', :descripcion => 'Tiene acceso a todo'
+b = Employee.create! :nombre => 'Michael King', :apellido_paterno => 'Gutierrez', :apellido_materno => 'Maldonado', :fecha_nacimiento => Date.new(1991,8,24), :genero => 'Masculino', :nacionalidad => 'Boliviano', :profesion => 'Estudiante', :especialidad => 'Cirujano', :fecha_alta => Date.today, :fecha_baja => Date.today, :CI => '8047284', :email => 'michael@gmail.com', :role_id => a.id
+c = User.create! :email => b.email, :password => b.CI, :password_confirmation => b.CI, :username => b.nombre, :employee_id => b.id
